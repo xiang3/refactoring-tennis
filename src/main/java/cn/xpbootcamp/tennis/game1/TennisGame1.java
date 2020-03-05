a536f1716f36eb9ad9e22d78ca47af3b5246e847
xiang3 <ZX814768>
2020-03-05 15:53:25 +0800

    将getScore方法进行抽取为三个方法

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -3,93 +3,78 @@ package cn.xpbootcamp.tennis.game1;
 import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame1 implements TennisGame {
-
-    private static final String LOVE_ALL = "Love-All";
-    private static final String FIFTEEN_ALL = "Fifteen-All";
-    private static final String THIRTY_ALL = "Thirty-All";
-    private static final String DEUCE = "Deuce";
-    private static final String ADVANTAGE_PLAYER_1 = "Advantage player1";
-    private static final String ADVANTAGE_PLAYER_2 = "Advantage player2";
-    private static final String WIN_FOR_PLAYER_1 = "Win for player1";
-    private static final String WIN_FOR_PLAYER_2 = "Win for player2";
-    private static final String STR = "-";
-    private static final String LOVE = "Love";
-    private static final String FIFTEEN = "Fifteen";
-    private static final String THIRTY = "Thirty";
-    private static final String FORTY = "Forty";
-    private Player player1 = new Player("player1");
-    private Player player2 = new Player("player2");
-
     private int mScore1 = 0;
     private int mScore2 = 0;
 
     public void wonPoint(String playerName) {
-        player1.updateScoreIfWon(playerName);
-        player2.updateScoreIfWon(playerName);
+
+        if (playerName.equals("player1"))
+            mScore1 += 1;
+        else
+            mScore2 += 1;
     }
 
     public String getScore() {
         StringBuilder score = new StringBuilder();
-        int tempScore = 0;
         if (mScore1 == mScore2) {
-            score = getFinishedScore();
+            score = getAllScore();
         } else if (mScore1 >= 4 || mScore2 >= 4) {
-            score = getWinner();
+            score = getPlayerInfo();
         } else {
-            getGoingScore(score);
+            getThreeRoundScore(score);
         }
         return score.toString();
     }
 
-    private void getGoingScore(StringBuilder score) {
+    private void getThreeRoundScore(StringBuilder score) {
         int tempScore;
         for (int i = 1; i < 3; i++) {
             if (i == 1) tempScore = mScore1;
             else {
-                score.append(STR);
+                score.append("-");
                 tempScore = mScore2;
             }
             switch (tempScore) {
                 case 0:
-                    score.append(LOVE);
+                    score.append("Love");
                     break;
                 case 1:
-                    score.append(FIFTEEN);
+                    score.append("Fifteen");
                     break;
                 case 2:
-                    score.append(THIRTY);
+                    score.append("Thirty");
                     break;
                 case 3:
-                    score.append(FORTY);
+                    score.append("Forty");
                     break;
             }
         }
     }
 
-    private StringBuilder getWinner() {
+    private StringBuilder getPlayerInfo() {
         StringBuilder score;
         int minusResult = mScore1 - mScore2;
-        if (minusResult == 1) score = new StringBuilder(ADVANTAGE_PLAYER_1);
-        else if (minusResult == -1) score = new StringBuilder(ADVANTAGE_PLAYER_2);
-        else if (minusResult >= 2) score = new StringBuilder(WIN_FOR_PLAYER_1);
-        else score = new StringBuilder(WIN_FOR_PLAYER_2);
+        if (minusResult == 1) score = new StringBuilder("Advantage player1");
+        else if (minusResult == -1) score = new StringBuilder("Advantage player2");
+        else if (minusResult >= 2) score = new StringBuilder("Win for player1");
+        else score = new StringBuilder("Win for player2");
         return score;
     }
 
-    private StringBuilder getFinishedScore() {
+    private StringBuilder getAllScore() {
         StringBuilder score;
         switch (mScore1) {
             case 0:
-                score = new StringBuilder(LOVE_ALL);
+                score = new StringBuilder("Love-All");
                 break;
             case 1:
-                score = new StringBuilder(FIFTEEN_ALL);
+                score = new StringBuilder("Fifteen-All");
                 break;
             case 2:
-                score = new StringBuilder(THIRTY_ALL);
+                score = new StringBuilder("Thirty-All");
                 break;
             default:
-                score = new StringBuilder(DEUCE);
+                score = new StringBuilder("Deuce");
                 break;
 
         }
